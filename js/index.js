f2ab0e4de2ab002f54b941d9eaa6b3efccd3eeab
BaguetteConnoisseur <Albert.sjoman@elev.ga.ntig.se>
2020-01-22 10:52:51 +0100

    V.4

---
+++ js/index.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function toggleClass(element, className) {
+    var classString = element.className, nameIndex = classString.indexOf(className);
+    if (nameIndex == -1) {
+        classString += ' ' + className;
+    } else {
+        classString = classString.substr(0, nameIndex) + classString.substr(nameIndex + className.length);
+    }
+    element.className = classString;
+}
