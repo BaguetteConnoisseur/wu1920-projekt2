d7db9af52d62317a6643fdddd759b1d4081ad93d
BaguetteConnoisseur <Albert.sjoman@elev.ga.ntig.se>
2020-03-20 22:10:31 +0100

    Hemma

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
-document.querySelector('#menu-button').addEventListener('click', function () {
-    document.querySelector('#main-menu').classList.toggle('active');
+document.querySelector('.menu-button').addEventListener('click', function () {
+    document.querySelector('.main-menu').classList.toggle('active');
 });
 
+document.querySelector('.menu-button2').addEventListener('click', function () {
+    document.querySelector('.main-menu2').classList.toggle('active');
+});
+
+document.querySelector('.menu-button3').addEventListener('click', function () {
+    document.querySelector('.main-menu3').classList.toggle('active');
+});
+
+document.querySelector('.menu-button4').addEventListener('click', function () {
+    document.querySelector('.main-menu4').classList.toggle('active');
+});
+
+document.querySelector('.menu-button5').addEventListener('click', function () {
+    document.querySelector('.main-menu5').classList.toggle('active');
+});
 
 
 
